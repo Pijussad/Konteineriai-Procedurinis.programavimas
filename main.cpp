c48277dcde48b73a6510908c33986dc52cc36b27
Pijussad <64306577+Pijussad@users.noreply.github.com>
2023-03-04 19:27:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main() {
   cout << "Ar norite nuskaityti duomenis iš failo? Jei taip, įveskite 1, jei ne, įveskite 2: ";
   do{
     cin >> pasirinkimas;
-    if (pasirinkimas != '1' && pasirinkimas != '2') cout << "Invalid choice. Please choose 1 or 2: ";
+    if (pasirinkimas != '1' && pasirinkimas != '2') cout << "Prasošome pasirinkti 1 arba 2:  ";
   }while(pasirinkimas != '1' && pasirinkimas != '2');
   
   if (pasirinkimas == '1') {
