ed4d9816bd5c2d37f81eb0585852e04664464171
Pijussad <64306577+Pijussad@users.noreply.github.com>
2023-03-04 16:54:16 +0200

    Add files via upload

---
+++ mylib-2.h
@@ -0,0 +1,37 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <limits>
+#include <random>
+#include <sstream>
+#include <algorithm>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::string;
+using std::copy;
+using std::fixed;
+using std::sort;
+using std::streamsize;
+using std::numeric_limits;
+using std::vector;
+using std::ifstream;
+using std::istringstream;
+using std::stringstream;
+
+
+
+
+#endif // MYLIB_H_INCLUDED
